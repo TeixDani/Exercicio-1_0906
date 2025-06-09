8f4d6d76cda6d518e2adab5b9ad51226380637e8
TeixDani <daniel15alexandre15@gmail.com>
2025-06-09 16:04:56 -0300

    Add files via upload

---
+++ Exercicio-2_0906.py
@@ -0,0 +1,13 @@
+class retangulo:
+
+    def area(largura, altura):
+        return largura * altura
+
+        resu = area()
+
+altura = float(input("Digite a altura: "))
+largura = float(input("Digite a largura: "))
+
+resultado = (largura * altura)
+
+print(resultado)
